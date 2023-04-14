d7971949c6f4b5fdcb9bbcd9ec57f6ffe1df3144
Pradeep <pansarepradeep15@gmail.com>
2023-04-14 13:28:57 -0700

    About Page Commit

--- app/src/main/java/com/example/a499_android/AboutActivity.java
+++ app/src/main/java/com/example/a499_android/AboutActivity.java
@@ -1,4 +1,5 @@
 package com.example.a499_android;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -8,34 +9,43 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import android.os.Bundle;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+public class AboutActivity extends AppCompatActivity {
 
-
-
-public class About extends AppCompatActivity{
-
-    Button About;
+    Button AboutMark;
 
     RadioGroup radioGroup;
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-
     DocumentReference docRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.about_activity);
+        setContentView(R.layout.activity_about);
 
-        about = findViewById(R.id.about);
-        radioGroup = findViewById()
+        AboutMark = findViewById(R.id.about_mark);
+
+        AboutMark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAboutActivity();
+            }
+        });
 
     }
+
+    public void openAboutActivity(){
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+
+    }
+}
